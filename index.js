7b309863d6a8d9ae60f1fca15632e590e0a142c5
excellent-response-1205 <valeriaramey@gmail.com>
2019-10-29 02:55:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  let sentence = [];
+  for(let i = 0; i < musicians.length; i+=2) {
+    let str = musicians[i] + ' plays ' + instruments[i];
+    
+    sentence.push(str);
+  }
+  return sentence;
+  
+}
+
+
