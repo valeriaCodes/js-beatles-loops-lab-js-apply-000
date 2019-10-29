339a52a9851a3926a826b9fc56dd4160ae80a3b5
excellent-response-1205 <valeriaramey@gmail.com>
2019-10-29 03:08:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay (musicians, instruments) {
   let sentence = [];
   for(let i = 0; i < musicians.length; i+=2) {
-    let str = musicians[i] + ' plays ' + instruments[i];
+    let str = musicians[i] + ' plays ' + instruments[i] + \n;
     
-    sentence.push(str) + \n;
+    sentence.push(str);
   }
   return sentence;
   
@@ -13,7 +13,7 @@ function johnLennonFacts(facts) {
   let newArr = [];
   i = 0
   while (i < facts.length) {
-    let str = facts[i] + '!!!";
+    let str = facts[i] + '!!!';
   }
 }
 
